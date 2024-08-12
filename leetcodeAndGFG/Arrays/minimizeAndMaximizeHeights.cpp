169a20f18258d60b767973df69b50133a3b515c4
Nikith-S <nikith391@gmail.com>
2024-08-12 14:37:03 +0530

    commit

--- leetcodeAndGFG/Arrays/minimizeAndMaximizeHeights.cpp
+++ leetcodeAndGFG/Arrays/minimizeAndMaximizeHeights.cpp
@@ -1,7 +1,7 @@
 int getMinDiff(int arr[], int n, int k)
 {
     // Sort the array
-    std::sort(arr, arr + n);
+    sort(arr, arr + n);
 
     // Initialize result
     int ans = arr[n - 1] - arr[0];
@@ -13,11 +13,11 @@ int getMinDiff(int arr[], int n, int k)
     // Traverse the array and find the minimum difference
     for (int i = 0; i < n - 1; i++)
     {
-        int mini = std::min(smallest, arr[i + 1] - k);
-        int maxi = std::max(largest, arr[i] + k);
+        int mini = min(smallest, arr[i + 1] - k);
+        int maxi = max(largest, arr[i] + k);
         if (mini < 0)
             continue;
-        ans = std::min(ans, maxi - mini);
+        ans = min(ans, maxi - mini);
     }
     return ans;
 }
