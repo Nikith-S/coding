99bd4a4dbda2c30464a83431462f1642c559cfbf
Nikith-S <nikith391@gmail.com>
2024-08-18 09:25:32 +0530

    sort 0 ,1  and 2's in a link list

---
+++ leetcodeAndGFG/LinkedList.cpp/sort0,1,2.cpp
@@ -0,0 +1,63 @@
+/*
+
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+public:
+    // Function to sort a linked list of 0s, 1s and 2s.
+    Node *segregate(Node *head)
+    {
+        int zeroCount = 0;
+        int oneCount = 0;
+        int twoCount = 0;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            if (temp->data == 0)
+            {
+                zeroCount++;
+            }
+            else if (temp->data == 1)
+            {
+                oneCount++;
+            }
+            else
+            {
+                twoCount++;
+            }
+            temp = temp->next;
+        }
+        temp = head;
+        while (temp != NULL)
+        {
+            if (zeroCount != 0)
+            {
+                temp->data = 0;
+                zeroCount--;
+            }
+            else if (oneCount != 0)
+            {
+                temp->data = 1;
+                oneCount--;
+            }
+            else if (twoCount != 0)
+            {
+                temp->data = 2;
+                twoCount--;
+            }
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
