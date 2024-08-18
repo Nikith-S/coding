d631095d73294a1e33b032bd6446715cd1a03122
Nikith-S <nikith391@gmail.com>
2024-08-18 09:19:18 +0530

    first Node in the loop

---
+++ leetcodeAndGFG/LinkedList.cpp/firstNodeInLoop.cpp
@@ -0,0 +1,38 @@
+Node *floydDetection(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    Node *fast = head;
+    Node *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        // Check for cycle
+        if (slow == fast)
+        {
+            return slow; // Correct placement of return
+        }
+    }
+    return NULL; // Return NULL if no cycle is detected
+}
+
+public:
+// Function to find the first node if the linked list has a loop.
+Node *findFirstNode(Node *head)
+{
+    Node *intersection = floydDetection(head);
+    if (intersection == NULL)
+    {
+        return NULL; // If no intersection, return NULL
+    }
+    Node *slow = head;
+    while (slow != intersection)
+    {
+        slow = slow->next;
+        intersection = intersection->next;
+    }
+    return slow; // Return the node itself
+}
