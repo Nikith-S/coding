e568a235e833bab3440e2fe3369cb3dad55313e7
Nikith-S <nikith391@gmail.com>
2025-04-10 13:56:56 +0530

    commit

--- javascriptOutputBasedQuestion/index.js
+++ javascriptOutputBasedQuestion/index.js
@@ -59,3 +59,222 @@
 // }
 
 // (false || hello)(); // Hello
+
+// ------------------------------------------------------------------------
+
+// myFunc();
+// var myFunc = function () {
+//   console.log("I am first");
+// };
+// myFunc();
+// function myFunc() {
+//   console.log("I am second");
+// }
+// myFunc();
+// I am second
+// I am first
+// I am first
+
+// -------------------------------------------------------------
+
+// myFunc();
+// function myFunc() {
+//   console.log("hello");
+// } // hello
+// ----------------------------------------------------------------
+
+// myfuc(); if i am myfuc() here it will be not hoisted it wiil throw an error like by saying myfuc is not and function
+// var myfuc = function () {
+//   console.log("hello");
+// };
+// myfuc();  for this the output will be like hello
+
+// -----------------------------------------------------------
+
+// console.log(a);
+// var a = 10;  //10
+// --------------------------------------------------------------------
+
+// myFunc();
+// function myFunc() {
+//   console.log("I am first");
+// }
+// myFunc();
+// function myFunc() {
+//   console.log("I am second");
+// }
+// myFunc();
+// I am second
+// I am second
+// I am second
+// --------------------------------------------------------------------------
+
+// An Example for function curring
+// function abc(a) {
+//   return function bc(b) {
+//     return a + b;
+//   };
+// }
+// console.log(abc(10)(20));
+// -------------------------------------------------------------
+// an example for infinite curring
+// function add(a) {
+//     return function (b) {
+//       if (b !== undefined) {
+//         return add(a + b);
+//       } else {
+//         return a;
+//       }
+//     };
+//   }
+//   // Usage:
+//   console.log(add(10)(20)(30)(40)()); // Output: 100
+// ----------------------------------------------------------------------------
+
+// // ------->first oreder function
+// 🔹 First-Order Function
+// A first-order function is a regular function that:
+
+// Does NOT accept another function as an argument
+
+// Does NOT return another function
+
+// 📌 Example:
+// javascript
+// Copy
+// Edit
+// function greet(name) {
+//   return "Hello, " + name;
+// }
+
+// In JavaScript, functions are first-class citizens, meaning:
+// const greet = function(name) {
+//     return "Hello, " + name;
+//   };
+
+//   const run = greet;
+//   console.log(run("Nikith"));
+
+// 🔹 First-Class Functions
+// This refers to a feature of the language itself.
+// ✅ You can:
+
+// Assign functions to variables
+
+// Pass functions as arguments
+
+// Return functions from other functions
+
+// Store them in objects/arrays
+
+// ------------> higher order function
+// 🔹 Higher-Order Function
+// A higher-order function (HOF) is a function that:
+
+// Takes one or more functions as arguments, or
+// Returns a function
+
+// 📌 Example 1 (Takes a function):
+
+// function higherOrder(callback) {
+//   const name = "nikith";
+//   return callback(name);
+// }
+// function example(name) {
+//   return "hello" + " " + name;
+// }
+// console.log(higherOrder(example)); // hello nikith
+
+// ------> example 2
+// function abc(x) {
+//   return function (y) {
+//     return x * y;
+//   };
+// }
+// console.log(abc(3)(4));
+
+// -----------------------------------------------------------------
+// exapmle for ternay operator
+
+// let age = 20;
+// age >= 20 ? console.log("can vote") : console.log("can't vote");
+
+// ---------------------------------------------------------
+
+// function union(arr1, arr2) {
+//   let i = 0;
+//   let j = 0;
+//   let unionArray = [];
+
+//   while (i < arr1.length && j < arr2.length) {
+//     if (arr1[i] < arr2[j]) {
+//       unionArray.push(arr1[i]);
+//       i++;
+//     } else if (arr1[i] > arr2[j]) {
+//       unionArray.push(arr2[j]);
+//     } else {
+//       unionArray.push(arr1[i]);
+//       i++;
+//       j++;
+//     }
+//   }
+//   while (i < arr1.length) {
+//     unionArray.push(arr1[i]);
+//     i++;
+//   }
+
+//   while (j < arr2.length) {
+//     unionArray.push(arr2[j]);
+//     j++;
+//   }
+
+//   return unionArray;
+// }
+
+// console.log(union([1, 2, 3, 4], [5, 6]));
+
+// by using built in function
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// // console.log(...arr1, ...arr2);
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+// -----------------------------------------------------------------------------------
+
+// let object = {
+//   a: "10",
+// };
+
+// let convert = JSON.stringify(object);
+// console.log(convert);
+
+/////----------------------------------------------
+// to check the array are equal uisng length and .every()
+
+// let arr1 = [1, 2];
+// let arr2 = [1, 2];
+// function isequal(arr1, arr2) {
+//   return (
+//     Array.isArray(arr1) === Array.isArray(arr2) &&
+//     arr1.length === arr2.length &&
+//     arr1.every((value, index) => value === arr2[index])
+//   );
+// }
+// console.log(isequal(arr1, arr2));
+
+// ----------------------------------------------------------------------------------
+// diif between event caputuring and event bubling
+
+// event capturing
+// document.getElementById("parent").addEventListener("click", () => {
+//   console.log("Capturing parent");
+// }, true);
+
+// ---------------------event bubbling
+// document.getElementById("outer").addEventListener("click", () => {
+//   console.log("Outer");
+// }, true); // capturing
+
+// document.getElementById("inner").addEventListener("click", () => {
+//   console.log("Inner");
+// }, false); // bubbling
